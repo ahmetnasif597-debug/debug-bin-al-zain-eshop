fc4b1345e47735a7aa78b4b7f8da26ae31b8e47b
ahmetnasif597-debug <ahmetnasif597@gmail.com>
2026-06-20 15:49:04 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location"; // إضافة الـ Hash Location
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -148,7 +149,7 @@ function App() {
         <StoreStatusProvider>
           <CartProvider>
             <AdminProvider>
-              <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+              <WouterRouter hook={useHashLocation}>
                 <Router />
               </WouterRouter>
               <Toaster />
