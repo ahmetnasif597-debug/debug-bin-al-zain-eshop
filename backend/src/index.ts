ac1552c74887c3168a85a04abe85d4e718f1dd02
ahmetnasif597-debug <ahmetnasif597@gmail.com>
2026-06-22 14:53:34 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,8 @@ import { logger } from "./lib/logger";
 
 const port = Number(process.env["PORT"] ?? 4000);
 
-app.listen(port, (err) => {
-  if (err) {
-    logger.error({ err }, "Error listening on port");
-    process.exit(1);
-  }
+app.listen(port, () => {
   logger.info({ port }, "Server listening");
 });
+
+export default app;
